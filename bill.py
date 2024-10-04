675c9792de6bfc100a7e3dcc107c4803837d5ca6
roxeg12 <roxana.e.gutierrez12@gmail.com>
2024-10-04 12:34:00 -0500

    Add split methods to BillSPlitter

--- bill.py
+++ bill.py
@@ -40,8 +40,19 @@ class Bill:
                 return item, True
         return None, False
     
+    def get_item_price(self, name):
+        item, found = self.get_item(name)
+        return item[1], found
+    
+    def get_item_quantity(self, name):
+        item, found = self.get_item(name)
+        return item[2], found
+
     def get_total(self):
         return self.total
+    
+    def get_subtotal(self):
+        return self.subtotal
 
     def add_item(self, item):
         if not self.find_item(item[0]): 
@@ -61,6 +72,7 @@ class Bill:
         item = [name, price, quantity]
         self.force_add_item(item)
     
+
     def delete_item(self, name):
         for item in self.data:
             if item[0] == name:
@@ -83,12 +95,19 @@ class Bill:
             self.people = []
             self.items = {}
             self.persons = {}
+            self.equal = False
+            self.percentage = False
+            self.byitem = False
         
         def __str__(self) -> str:
             pass
 
-        def split_equal(self, num_people):
-            return self.bill.get_total() / num_people
+        def split_equal(self, people):
+            share = self.bill.get_total() / len(people)
+            shares = {}
+            for person in people:
+                shares[person] = share
+            return shares, True
 
         def add_person(self, name):
             if name not in self.people:
@@ -102,8 +121,70 @@ class Bill:
                 self.people.remove()
                 return True
             return False
-         
+        
+        def reset_split_method(self):
+            self.equal = False
+            self.percentage = False
+            self.byitem = False
+            self.persons = {}
+            self.items = {}
 
+
+        def set_split_method(self, method, data):
+            self.reset_split_method()
+            if method == "equal":
+                self.equal = True
+                self.people = data
+                return True
+            elif method == "percentage":
+                self.percentage = True
+                self.persons = data["persons"]
+                
+                return True
+            elif method == "by item":
+                self.byitem = True
+                self.persons = data["persons"]
+                self.items = data["items"]
+                return True
+            else:
+                return False
+            
+        def split_percentage(self):
+            bill_total = self.bill.get_total()
+            shares = {}
+            for person in self.persons.keys():
+                percent = self.persons[person]
+                shares[person] = bill_total * percent
+            return shares
+        
+        def split_items(self):
+            bill_difference = self.bill.get_total() - self.bill.get_subtotal()
+            equal_share = bill_difference / len(self.persons)
+            shares = {}
+            item_shares = {}
+            for item in self.items.keys():
+                if self.bill.find_item(item):
+                    item_price, found = self.bill.get_item_price(item)
+                    item_share = item_price / self.items[item]
+                    item_shares[item] = item_share
+                else:
+                    return {}, False
+            
+            
+            for person in self.persons.keys():
+                
+                share = 0
+                item_list = self.persons[person]
+                for item in item_list:
+                    if item in item_shares:
+                        share += item_shares[item]
+                    else:
+                        return {}, False
+                shares[person] = share + equal_share
+
+            return shares, True
+
+        
          #method 1: split by person - each person picks what they ate from, each of those items is split evenly among the people that
          #          picked it, each person pays the total of their shares of the items they picked
          #method 2: split by percentage - each person is assigned a percentage of the meal to pay, the total is split according to
